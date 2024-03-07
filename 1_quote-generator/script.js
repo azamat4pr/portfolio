1032ba918604b7d4977e2a4345b9ab535699e1fc
Jauysh <a.jauysh@gmail.com>
2024-03-07 15:25:15 +0200

    semifinal

--- 1_quote-generator/script.js
+++ 1_quote-generator/script.js
@@ -1,10 +1,29 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote(){
     // Pick a random quote from apiQuotes array
-    const random_quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(random_quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if the author is unknown
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+    // Check if the text is long
+    if ( quote.text.length > 110 ) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 
@@ -20,6 +39,15 @@ async function getQuotes(){
     }
 }
 
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners 
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
