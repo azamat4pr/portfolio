4b9f79cd486a7b857b34102e7449de4f402a9f09
Jauysh <a.jauysh@gmail.com>
2024-03-07 14:39:00 +0200

    random quote fetching

--- 1_quote-generator/script.js
+++ 1_quote-generator/script.js
@@ -1,2 +1,25 @@
+let apiQuotes = [];
 
-console.log("hello 2");
+// Show New Quote
+function newQuote(){
+    // Pick a random quote from apiQuotes array
+    const random_quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(random_quote);
+}
+
+
+// Get quotes from API
+async function getQuotes(){
+    const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes =  await response.json();
+        newQuote();
+    } catch(error) {
+        // Carch errors here
+    }
+}
+
+
+// On load
+getQuotes();
